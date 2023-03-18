7a33d14b504bb647cf490a1a05ad3f5d614c4d16
Damien Koh <damienkwk@hotmail.sg>
2023-03-18 14:03:24 +0800

    flatpickr

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
+
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :new ]
 
   def home
+    @trip = Trip.new
+  end
+
+  def new
+    require "open-uri"
+    require "nokogiri"
+    location = "perth"
+    url = "https://www.klook.com/en-SG/experiences/search/?keyword=#{location}&size=24"
+    p url
+    html_file = URI.open(url).read
+    @html_doc = Nokogiri::HTML.parse(html_file)
   end
 end
